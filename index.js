11c34971e9dcc95bcd8e5096469a1b22630ed099
Ataurrab9495 <mohammedataurrab@gmail.com>
2021-07-10 13:13:29 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 
-document.body.addEventListener('keydown',function(e){
-    var key = e.keycode;
-    
-    console.log(key +'key is Pressed');
+
+function play(name){
+    let track = document.getElementById(name);
+    track.play();
+}
+window.addEventListener('keydown',function(e){
+    console.log('hii')
+    let key = e.keyCode;
+    if(key === 65){
+        play('A');
+    } else if(key === 83){
+        play('S')
+    } else if(key === 68){
+        play('D')
+    } else if(key === 70){
+        play('F')
+    } else if(key === 71){
+        play('G')
+    } else if(key === 72){
+        play('H')
+    } else if(key === 74){
+        play('J')
+    } else if(key === 75){
+        play('K')
+    } else if(key === 76){
+        play('L')
+    }
+});
+
+/* document.body.addEventListener('keypress',function(e){
+    var key = e.keyCode;
+    if(key === 13){
+        console.log('keypress for => ' + key);
+    }
     
 });
+
+document.body.addEventListener('keyup',function(e){
+    var key = e.keyCode;
+    if(key === 13){
+        console.log('keyup for => ' + key);
+    }
+    
+}); */
